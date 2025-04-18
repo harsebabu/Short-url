dc45acdb9aadf2a112cefd5cafb15bad20b1a7cf
Rahul Kumar patel <143025746+harsebabu@users.noreply.github.com>
2025-04-18 21:30:00 +0530

    Add files via upload

--- url.js
+++ url.js
@@ -1,18 +1,4 @@
-const mongoose=require("mongoose");
 
-const urlSchema=new mongoose.Schema({
-    shortID:{
-        type:String,
-        require:true,
-    },
-    redirectID:{
-        type:String,
-        require:true,
-    },
-    visiblehistory:
-        [{timestamp:{type:Number}}],
-},
-{timestamps:true}
-);
-const url=mongoose.model("url",urlSchema)
-module.exports=url;
+const express=require("express");
+const router=express.Router();
+router.post("/");
